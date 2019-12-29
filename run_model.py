f7d149f5fcb017c412ecb93225de5cb98981e1d9
Demetri Pananos <dpananos@gmail.com>
2019-12-29 00:47:01 -0500

    add shebang to run_model

--- run_model.py
+++ run_model.py
@@ -1,3 +1,5 @@
+#! /Users/demetri/opt/miniconda3/envs/gymbo/bin/python
+
 from tools.db_tools import update_tweet_db, update_weather_db
 from tools.data_tools import fetch_wr
 from tools.plot_tools import tweet, make_plot, fetch_predictions
