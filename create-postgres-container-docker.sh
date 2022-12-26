f7cff454f6c52500ea386f4fe93368ea4525ef1c
Demetri Pananos <dpananos@gmail.com>
2022-12-26 14:52:42 -0500

    Removes docer script

--- create-postgres-container-docker.sh
+++
@@ -1,7 +0,0 @@
-docker run \
-  --name postgres-db \
-  -e POSTGRES_PASSWORD=postgres \
-  -e POSTGRES_USERNAME=postgres \
-  -d \
-  -v ${PWD}/postgres-docker:/var/lib/postgresql/data \
-  -p 5432:5432 postgres 
