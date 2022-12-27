009cff50d0a9a569b97bb871cd7238f977719c6d
Demetri Pananos <dpananos@gmail.com>
2022-12-26 19:13:31 -0500

    Adds SQL connection class

---
+++ gymbro/connect.py
@@ -0,0 +1,39 @@
+import psycopg2
+import os
+from dotenv import dotenv_values
+from dataclasses import dataclass
+
+@dataclass
+class SqlConnection:
+
+    def __init__(self, host, port, database, user, password):
+        self.host = host
+        self.port = port
+        self.database = database
+        self.user = user
+        self.password = password
+
+    def connect(self):
+        return psycopg2.connect(
+            host=self.host,
+            port=self.port,
+            database=self.database,
+            user=self.user,
+            password=self.password,
+        )
+    
+    @classmethod
+    def from_env(cls, file_path=None):
+
+        if not file_path:
+            file_path = '.env'
+
+        config = dotenv_values(file_path)
+
+        return cls(
+            host=config["DB_HOST"],
+            port=config["DB_PORT"],
+            database=config["DB_NAME"],
+            user=config["DB_USER"],
+            password=config["DB_PASSWORD"],
+        )
